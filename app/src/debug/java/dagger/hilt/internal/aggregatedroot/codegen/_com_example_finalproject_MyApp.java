7c6e4ea78f1ad6ad820b827f7381180f7afe5124
UldanaKanal <120903029+UldanaKanal@users.noreply.github.com>
2023-12-19 02:09:12 +0600

    adding some files

---
+++ app/src/debug/java/dagger/hilt/internal/aggregatedroot/codegen/_com_example_finalproject_MyApp.java
@@ -0,0 +1,19 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+        root = "com.example.finalproject.MyApp",
+        rootPackage = "com.example.finalproject",
+        originatingRoot = "com.example.finalproject.MyApp",
+        originatingRootPackage = "com.example.finalproject",
+        rootAnnotation = HiltAndroidApp.class,
+        rootSimpleNames = "MyApp",
+        originatingRootSimpleNames = "MyApp"
+)
+public class _com_example_finalproject_MyApp {
+}
---
+++ app/src/debug/java/hilt_aggregated_deps/_com_example_finalproject_di_DbModule.java
@@ -0,0 +1,4 @@
+package hilt_aggregated_deps;
+
+public class _com_example_finalproject_di_DbModule {
+}
